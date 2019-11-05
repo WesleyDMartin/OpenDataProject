848d551e8eeabbca1b4ec1aed26b2ef794cbab19
Wesley Martin <wdmartincc@gmail.com>
2019-11-05 13:22:20 +0000

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-# require 'csv' 
-# @location_set = LocationSet.create(name: "ION_Stops")
-# csv_text = File.read('public/ION_Stops.csv')
-# csv = CSV.parse(csv_text, :headers => true)
-# csv.each do |row|
-#   @location_set.locations.create(lat: row[1], long: row[0])
-# end
+require 'csv' 
+@location_set = LocationSet.create(name: "ION_Stops")
+csv_text = File.read('public/ION_Stops.csv')
+csv = CSV.parse(csv_text, :headers => true)
+csv.each do |row|
+  @location_set.locations.create(lat: row[1], long: row[0])
+end
 
-# @permit_set = LocationSet.create(name: "Building_Permits")
-# csv_text2 = File.read('public/FormattedLocations.csv')
-# csv2 = CSV.parse(csv_text2, :headers => true)
-# csv2.each do |row|
-#   @permit_set.locations.create(row.to_hash)
-# end
+@permit_set = LocationSet.create(name: "Building_Permits")
+csv_text2 = File.read('public/FormattedLocations.csv')
+csv2 = CSV.parse(csv_text2, :headers => true)
+csv2.each do |row|
+  @permit_set.locations.create(row.to_hash)
+end
